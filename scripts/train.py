80bfeff091ae47cc9a173aca7d010c3966609bd9
Super <eboakye1000@gmail.com>
2025-09-06 21:04:56 +0000

    "fit mlr for clarity sensor,  for both wet and dry periods"

--- scripts/train.py
+++ scripts/train.py
@@ -45,7 +45,7 @@ if __name__ == "__main__":
     
     # file path must be changed to the location of the data
     df = pd.read_csv(
-        r"C:\Users\USER\Documents\Afri-SET\Zefan Wet\zefan_ref_wet.csv",
+        r"C:\Users\USER\Documents\Afri-SET\Calibrations\clarity_dry.csv",
         parse_dates=["date"],
         index_col="date"
     )
@@ -83,10 +83,10 @@ if __name__ == "__main__":
     features = df[["pm25","temp", "rh"]]
     
     #the period for the calibration must be added to the sensor name, say sensit_ramp_wet
-    mlr_calibration(df, features, target, sensor_name="Zefan_Wet")
+    mlr_calibration(df, features, target, sensor_name="Clarity_Dry")
     
     #Be sure the state the sensor name
-    sensor_name = "Zefan_Wet" 
+    sensor_name = "Clarity_Dry" 
     outdir = r"C:\Users\USER\Documents\Afri-SET\Calibrations\data"
     df.to_csv(f"{outdir}/{sensor_name}_calibrated_data.csv")
     
