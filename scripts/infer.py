4c7495fc7133767aa7c34e8323b8b68f6fd3bb67
Super <eboakye1000@gmail.com>
2025-09-06 21:06:13 +0000

    "inference for correcting clarity data for respective sites for both wet and dry periods"

---
+++ scripts/infer.py
@@ -0,0 +1,45 @@
+import joblib
+import pandas as pd
+
+#loading the model for dry period
+model_dry = joblib.load(r"C:\Users\USER\Documents\Afri-SET\Calibrations\models\Clarity_Dry_mlr_model.joblib")
+model_wet = joblib.load(r"C:\Users\USER\Documents\Afri-SET\Calibrations\models\Clarity_Wet_mlr_model.joblib")
+
+df_dry = pd.read_csv(r"C:\Users\USER\Documents\Afri-SET\Calibrations\TBC_dry.csv",
+                 parse_dates=['date'],
+                 index_col = "date")
+
+df_wet = pd.read_csv(r"C:\Users\USER\Documents\Afri-SET\Calibrations\TBC_wet.csv",
+                 parse_dates=['date'],
+                 index_col = "date")
+
+sites = ["CPC", "St", "Bet", "Com", "TMA", "Fre", "Val",
+         "LEK", "Chal", "Ade", "San", "Adu", "Sua", "Sep",
+         "Aso", "Gui"]
+
+for site in sites:
+    pm25 = f"{site}_pm25"
+    temp = f"{site}_temp"
+    rh = f"{site}_rh"
+    
+    if all(col in df_dry.columns for col in [pm25, temp, rh]):
+        
+        X_dry = df_dry[[pm25, temp, rh]].dropna()
+        X_dry = X_dry.rename(columns={pm25: "pm25", temp: "temp", rh: "rh"})
+        corrected_dry = model_dry.predict(X_dry)
+        df_dry.loc[X_dry.index, f"{site}_corrected"] = corrected_dry
+        
+    if all (col in df_wet.columns for col in [pm25, temp, rh]):   
+        
+        X_wet = df_wet[[pm25, temp, rh]].dropna()
+        X_wet = X_wet.rename(columns={pm25: "pm25", temp: "temp", rh: "rh"})
+        corrected_wet = model_wet.predict(X_wet)
+        df_wet.loc[X_wet.index, f"{site}_corrected"] = corrected_wet
+
+    else:
+        print(f"Missing columns for site {site}. Skipping...")
+        
+df_dry.to_csv(r"C:\Users\USER\Documents\Afri-SET\Calibrations\clarity_dry_corrected.csv", index = False)
+df_wet.to_csv(r"C:\Users\USER\Documents\Afri-SET\Calibrations\clarity_wet_corrected.csv", index = False)
+
+print("Inference completed and results saved.")
